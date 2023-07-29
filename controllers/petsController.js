99e6ca249132c3380ac5617678c0e9742d8360e9
ANAND VIBHUTI <mailaddress.anandvibhuti@gmail.com>
2023-07-29 23:04:40 +0530

    pets routes outline.

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,7 +1,29 @@
-const pets=require('../repositories/petsRepository');
-class PetsController{
-static index(req,res){
-    res.send('pets');
+const pets = require("../repositories/petsRepository");
+class PetsController {
+  static index(req, res) {
+    res.send("pets");
+  }
+  static search(req, res) {
+    res.send("pets search");
+  }
+  static filter(req, res) {
+    res.send("pets filter");
+  }
+  static profile(req, res) {
+    res.send("pets profile");
+  }
+  static new(req, res) {
+    res.send("pets new");
+  }
+  static edit(req, res) {
+    res.send("pets edit");
+  }
+  static new_pet(req, res) {
+    res.send("pets new post");
+  }
+  static edit_pet(req,res){
+    res.send('pets edit post');
+  }
 }
-}
-module.exports=PetsController
+
+module.exports = PetsController;
--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,13 @@
-const express=require('express');
-const router=express.Router();
-const petsController=require('../controllers/petsController');
-router.get('/',petsController.index);
+const express = require("express");
+const router = express.Router();
+const petsController = require("../controllers/petsController");
+router.get("/", petsController.index);
+router.get("/search", petsController.search);
+router.get("/filter", petsController.filter);
+router.get("/profile", petsController.profile);
+router.get("/new", petsController.new);
+router.get("/edit/:pet", petsController.edit);
+router.post("/new", petsController.new_pet);
+router.post("/edit/:pet", petsController.edit_pet);
 
-module.exports=router;
+module.exports = router;
