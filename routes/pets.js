c17024a2d27d2957ff385ec5dc23ee60195ecb0e
ANAND VIBHUTI <mailaddress.anandvibhuti@gmail.com>
2023-07-31 20:48:25 +0530

    search post route.

--- routes/pets.js
+++ routes/pets.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const petsController = require("../controllers/petsController");
 router.get("/", petsController.index);
 router.get("/search", petsController.search);
-
-router.get("/profile", petsController.profile);
+router.post("/search",petsController.search);
+router.get("/profile/:id", petsController.profile);
 router.get("/new", petsController.new);
 router.get("/edit/:pet", petsController.edit);
 router.post("/new", petsController.new_pet);
 router.post("/edit/:pet", petsController.edit_pet);
-router.delete('/profile/delete',petsController.delete_pet)
+router.delete('/profile/:id/delete',petsController.delete_pet);
 
 module.exports = router;
